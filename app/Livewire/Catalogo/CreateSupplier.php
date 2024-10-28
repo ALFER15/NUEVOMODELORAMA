1f18a317df98d8a4ff577e35a188d555c5e1f040
ALFER15 <aarias22@alumnos.uaq.mx>
2024-10-28 13:01:22 -0600

    quedo el supplier pendiente branch

--- app/Livewire/Catalogo/CreateSupplier.php
+++ app/Livewire/Catalogo/CreateSupplier.php
@@ -4,15 +4,19 @@ namespace App\Livewire\Catalogo;
 
 use App\Models\Supplier;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CreateSupplier extends Component
 {
+    use WithPagination;
 
     public $name, $phone, $address, $rfc, $email, $manager_name;
-    public $suppliers;
     public $idEditable;
 
     public $mEdit = false;
+    public $mCreate = false;
+
+    public $searchSup = '';
     public $supplierEdit = [
         'id' => '',
         'name' => '',
@@ -20,14 +24,19 @@ class CreateSupplier extends Component
         'address' => '',
         'email' => '',
         'manager_name' => '',
-        'rfc' => ''];
+        'rfc' => ''
+    ];
 
     public function render()
     {
-        $this->suppliers = Supplier::all();
+        $suppliers = Supplier::where('name', 'like', '%' . $this->searchSup . '%')
+            ->orWhere('rfc', 'like', '%' . $this->searchSup . '%')
+            ->orWhere('phone', 'like', '%' . $this->searchSup . '%')
+            ->orWhere('address','like', '%' . $this->searchSup . '%')
+            ->orderBy('id', 'desc')
+            ->paginate(5);
 
-        return view('livewire.catalogo.create-supplier');
-
+        return view('livewire.catalogo.create-supplier', compact('suppliers'));
     }
     
     public function enviar(){
@@ -39,7 +48,7 @@ class CreateSupplier extends Component
         $supplier->manager_name = $this->manager_name;
         $supplier->rfc = $this->rfc;
         $supplier->save();
-        $this->reset(['name','phone','address','rfc','email','manager_name']);
+        $this->reset(['name', 'phone', 'address', 'rfc', 'email', 'manager_name', 'mCreate']);
     }
     
     public function editar($supplierID){
@@ -54,24 +63,36 @@ class CreateSupplier extends Component
         $this->supplierEdit['manager_name'] = $supplierEditable->manager_name;
     }
 
-public function update() {
-    $supplier = Supplier::find($this->idEditable);
-    $supplier->update([
-        'name' => $this->supplierEdit['name'],
-        'phone' => $this->supplierEdit['phone'],
-        'address' => $this->supplierEdit['address'],
-        'rfc' => $this->supplierEdit['rfc'],
-        'email' => $this->supplierEdit['email'],
-        'manager_name' => $this->supplierEdit['manager_name'],
-    ]);
-    $this->reset([
-        'supplierEdit',
-        'idEditable',
-        'mEdit'
-    ]);
-}
+    public function update() {
+        $supplier = Supplier::find($this->idEditable);
+        $supplier->update([
+            'name' => $this->supplierEdit['name'],
+            'phone' => $this->supplierEdit['phone'],
+            'address' => $this->supplierEdit['address'],
+            'rfc' => $this->supplierEdit['rfc'],
+            'email' => $this->supplierEdit['email'],
+            'manager_name' => $this->supplierEdit['manager_name'],
+        ]);
+        $this->reset([
+            'supplierEdit',
+            'idEditable',
+            'mEdit'
+        ]);
+    }
+
+    public function formCancel(){
+        $this->reset([
+            'mCreate',
+            'name',
+            'phone',
+            'address',
+            'rfc',
+            'email',
+            'manager_name'
+        ]);
+    }
 
     public function eliminar (Supplier $supplier){
         $supplier->delete();
-        }
+    }
 }
--- resources/views/livewire/catalogo/create-supplier.blade.php
+++ resources/views/livewire/catalogo/create-supplier.blade.php
@@ -1,29 +1,45 @@
 <div>
     <span>Agregar supplier nuevo</span>
-    <div class="ml-2 mr-4 mb-4 mt-4">
-        <form class="max-w-sm mx-auto" wire:submit.prevent='enviar'>
-            <x-label for="name" value="Nombre"/>
-            <x-input name="name" wire:model='name'/>
+    
+    {{-- Modal de creación --}}
+    @if ($mCreate)
+    <div class="bg-gray-800 bg-opacity-25 fixed inset-0 flex justify-center items-center z-50">
+        <div class="bg-white shadow-lg rounded-lg p-6 max-w-md mx-auto">
+            <form class="max-w-sm mx-auto" wire:submit.prevent='enviar'>
+                <x-label for="name" value="Nombre"/>
+                <x-input name="name" wire:model='name'/>
 
-            <x-label for="phone" value="Teléfono"/>
-            <x-input name="phone" class="mb-2" wire:model='phone'/>
+                <x-label for="phone" value="Teléfono"/>
+                <x-input name="phone" class="mb-2" wire:model='phone'/>
 
-            <x-label for="email" value="Email"/>
-            <x-input name="email" wire:model='email'/>
+                <x-label for="email" value="Email"/>
+                <x-input name="email" wire:model='email'/>
 
-            <x-label for="manager_name" value="Nombre del representante"/>
-            <x-input name="manager_name" wire:model='manager_name'/>
+                <x-label for="manager_name" value="Nombre del representante"/>
+                <x-input name="manager_name" wire:model='manager_name'/>
 
-            <x-label for="address" value="Dirección"/>
-            <textarea id="address" name="address" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model='address' placeholder="Dirección"></textarea>
+                <x-label for="address" value="Dirección"/>
+                <textarea id="address" name="address" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model='address' placeholder="Dirección"></textarea>
 
-            <x-label name="rfc" value="RFC"/>
-            <x-input name="rfc" wire:model='rfc'/><br>
+                <x-label for="rfc" value="RFC"/>
+                <x-input name="rfc" wire:model='rfc'/><br>
 
-            <x-button class="mt-2">Guardar</x-button>
-        </form>    
+                <x-button class="mt-2">Guardar</x-button>
+                <x-border-button class="border-red-500 text-red-500 hover:bg-red-500 mt-2" wire:click='formCancel'>Cancelar</x-border-button>
+            </form>    
+        </div>
+    </div>
+    @endif
+
+    {{-- Campo de búsqueda y botón de agregar --}}
+    <div class="m-4">
+        <div class="flex justify-end items-center space-x-4">
+            <x-input name="search-supplier" placeholder="Buscar supplier" wire:model.live="searchSup" />
+            <x-button wire:click="$set('mCreate', true)">Agregar supplier</x-button>
+        </div>
     </div>
 
+    {{-- Tabla de suppliers --}}
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -58,7 +74,11 @@
         </tbody>
     </table>
 
-    <!-- Modal de edición -->
+    <div class="m-2">
+        {{ $suppliers->links() }}
+    </div>
+
+    {{-- Modal de edición --}}
     @if ($mEdit)
     <div class="bg-gray-800 bg-opacity-25 fixed inset-0 flex justify-center items-center">
         <div class="bg-white shadow rounded-lg p-6">
